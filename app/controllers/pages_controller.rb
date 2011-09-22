cde223bfd1a04576b90ce7780574588aaa4b5fa4
TakuyaKitamura <t.kitamura@tbs-v.co.jp>
2011-09-23 03:05:40 +0900

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,15 @@
 class PagesController < ApplicationController
   def home
      #automatically render the view "home.html.erb"
+     @title = "Home"
   end
 
   def contact
+     @title = "Contact"
+  end
+
+  def about
+     @title = "About"
   end
 
 end
