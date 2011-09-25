51e08c1fd128364a7cfed9d66691bb4f90eea2e7
TKitamura <t.kitamura@tbs-v.co.jp>
2011-09-25 15:40:49 +0900

    done by 7.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@
 #  updated_at :datetime
 #
 
+require 'digest'
 class User < ActiveRecord::Base
  
  #This create a virtual password attribute
@@ -27,6 +28,38 @@ class User < ActiveRecord::Base
  #Automatically create the virtual attribute 'password_confirmation'
  validates :password, :presence => true,
                       :confirmation => true,
-                      :length => { :within => 6..41 }
+                      :length => { :within => 6..40 }
+
+  before_save :encrypt_password
+
+  #Return true if the users password matches the submitted password.
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  def self.authenticate(email,submitted_password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+
+  private
+   
+    def encrypt_password
+      self.salt = make_salt unless has_password?(password) 
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+       secure_hash("#{salt}--#{string}")
+    end
+
+    def make_salt
+       secure_hash("#{Time.now.utc}--#{password}")
+    end
+
+    def secure_hash(string)
+       Digest::SHA2.hexdigest(string)
+    end
 
 end
