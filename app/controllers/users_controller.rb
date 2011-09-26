1318c235c337bf582cc827b4c37a35f73085b9ab
TakuyaKitamura <t.kitamura@tbs-v.co.jp>
2011-09-26 10:36:54 +0900

    done by 8.2.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,23 @@
 class UsersController < ApplicationController
   def new
     @title = "Sign up"
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
     @title = @user.name
+  end
+
+  def create
+    #:user is hash {:name => ,:email=> ,,,}from input name="user[OOO]"
+    @user = User.new(params[:user])
+    if @user.save
+      #Handle a successful save.
+    else
+      @title = "sign up"
+      render "new"
+    end
   end
 
 end
